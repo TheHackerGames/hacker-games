218e58cbc12514c7288cb5d0ea46eb4858585294
amine.benhallouk <amine.benhallouk@asos.com>
2018-01-19 10:28:24 +0000

    add profile changes and quizes and courses

---
+++ EXServiceHub/EXServiceHub/Controllers/CoursesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace EXServiceHub.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EXServiceHub/EXServiceHub/Controllers/QuizesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EXServiceHub.Controllers
+{
+    public class QuizesController : Controller
+    {
+        // GET: Quizes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
